785447ed170612b26baee32be33b7ea8139525dd
Akshay Mehta <mehta.akshay@gmail.com>
2022-02-26 08:24:49 +0000

    WIP Fix Biometric List View

--- biometric_attendance/biometric_attendance/doctype/biometric_attendance/biometric_attendance_list.js
+++ biometric_attendance/biometric_attendance/doctype/biometric_attendance/biometric_attendance_list.js
@@ -1,15 +1,20 @@
 frappe.listview_settings['Biometric Attendance'] = {
-    onload: function(me) {
-                me.page.add_menu_item(__("Delete Duplicate Attendance"), function() {
+        onload: function (me) {
+                me.page.add_menu_item(__("Delete Duplicate Attendance"), function () {
                         frappe.call({
                                 method: "biometric_attendance.biometric_attendance.utils.delete_duplicate_rows_from_attendance",
-                                callback: function(r) {
+                                callback: function (r) {
                                         if (!r.exc) {
                                                 frappe.msgprint("Success");
                                         }
                                 }
                         });
                 });
-    }
+        },
+        get_indicator: function (doc) {
+                //Override Get Indicator as Integer value of "status" field gives error
+                //https://github.com/frappe/frappe/pull/2660
+                return [];
+        }
 }
 
